edada9bcda1fc7a4f537c6d3f50f09616f630f15
SathishPalleni <118447414+SathishPalleni@users.noreply.github.com>
2026-01-27 13:35:51 +0530

    Time: 791 ms (58.87%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1541-top-travellers/top-travellers.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+select name, IFNULL(sum(distance),0) as travelled_distance 
+from Users as u left join Rides as r on u.id=r.user_id
+ group by u.id order by travelled_distance DESC, name;
