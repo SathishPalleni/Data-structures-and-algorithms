736842e9a4ab53ea94a54e5cda04842ef24ecc75
SathishPalleni <118447414+SathishPalleni@users.noreply.github.com>
2026-05-25 22:40:17 +0530

    Time: 2 ms (87.74%) | Memory: 48 MB (22.83%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int ditances = 0;
+        for(int i =0; i<nums.length; i++){
+            if(i>ditances ) return false;
+            ditances = Math.max(ditances, i+nums[i]);
+        }
+        return true;
+    }
+}
